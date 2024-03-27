0c53c689a4db1cbbcc52ef45663512c8077f86b2
richardpaulraj <richardpaulraj2@gmail.com>
2024-03-27 14:20:26 +0530

    Added comments

--- canvas.js
+++ canvas.js
@@ -15,12 +15,12 @@ const DrawingApp = {
   currentPencilColor: 'red',
   currentPencilWidth: 3,
   is3DView: false,
-  optionsFlag: true,
+  optionsFlag: false,
   pencilFlag: false,
 
   init() {
     this.tool = this.canvas2d.getContext('2d')
-    this.setupEventListeners()
+    this.setupEventListeners() //Calls the setupEventListeners() method of the DrawingApp object. 'this' refers to the DrawingApp object.
     this.setupToolsEventListeners()
     this.draw2DScene()
   },
@@ -34,6 +34,15 @@ const DrawingApp = {
 
     const redoButton = document.querySelector('.redo')
     redoButton.addEventListener('click', () => this.redo())
+
+    //So why we are using bind() method here:
+    // It does not immediately invoke the function but returns a new function with the specified this value.
+    //It's commonly used when you want to create a function with a specific this context to be used later, 
+
+    // call() method : It immediately invokes the function with the specified this value and arguments.
+
+    // This bind() method creates a new function that, when called, has this set to the value passed as an argument (in this case, this, which refers to the DrawingApp object).
+    // This ensures that inside the handleMouseDown method, this refers to the DrawingApp object, even when called as an event listener.
 
     this.canvas2d.addEventListener('mousedown', this.handleMouseDown.bind(this))
     this.canvas2d.addEventListener('mousemove', this.handleMouseMove.bind(this))
@@ -65,6 +74,9 @@ const DrawingApp = {
     })
   },
 
+
+
+  //To show and hide tools container
   toggleOptions(toolsCont, pencilToolCont) {
     this.optionsFlag = !this.optionsFlag
     const optionsCont = document.querySelector('.options-cont')
@@ -81,7 +93,11 @@ const DrawingApp = {
       toolsCont.style.display = 'flex'
     }
   },
+  //To show and hide tools container
 
+
+
+  // Pencil Event Handling Methods//
   togglePencilTool(pencilToolCont) {
     this.pencilFlag = !this.pencilFlag
     pencilToolCont.style.display = this.pencilFlag ? 'block' : 'none'
@@ -94,7 +110,12 @@ const DrawingApp = {
   updatePencilColor(color) {
     this.currentPencilColor = color
   },
+  // Pencil Event Handling Methods//
 
+
+
+
+  // Mouse Event Handling Methods//
   handleMouseDown(e) {
     this.startX = e.clientX
     this.startY = e.clientY
@@ -105,6 +126,8 @@ const DrawingApp = {
     if (this.isDrawing) {
       const endX = e.clientX
       const endY = e.clientY
+      //This function is responsible for drawing temporary lines on the canvas while the user is actively drawing.
+      //Once the user completes the drawing action, the temporary lines drawn by this function are cleared or replaced by the final drawn lines using drawLine function.
       this.drawTempLine(this.startX, this.startY, endX, endY)
     }
   },
@@ -113,19 +136,37 @@ const DrawingApp = {
     if (this.isDrawing) {
       const endX = e.clientX
       const endY = e.clientY
+      //Unlike drawTempLine, which is used for temporary visual feedback during drawing, drawLine adds the actual line to the canvas drawing.
       this.drawLine(this.startX, this.startY, endX, endY)
       this.isDrawing = false
     }
   },
+  // Mouse Event Handling Methods//
 
+
+
+// Toggles between 2D and 3D views.
   toggleView() {
     this.is3DView = !this.is3DView
+
+    // console.log(this.lines)
+
+    const toolsCont = document.querySelector('.tools-cont')
+    const pencilToolCont = document.querySelector('.pencil-tool-cont')
+
     if (this.is3DView) {
+
+      if (!this.optionsFlag) {
+        this.toggleOptions(toolsCont, pencilToolCont)
+      }
+      
       this.canvas2d.style.display = 'none'
       this.canvas3d.style.display = 'block'
       this.remove2DEventListeners()
       this.draw3DScene()
     } else {
+      this.toggleOptions(toolsCont, pencilToolCont)
+
       this.canvas3d.style.display = 'none'
       this.canvas2d.style.display = 'block'
       this.remove3DScene()
@@ -133,34 +174,56 @@ const DrawingApp = {
       this.redrawCanvas()
     }
   },
+  // Toggles between 2D and 3D views.
 
+
+  // Removes event listeners for 2D canvas.
   remove2DEventListeners() {
     this.canvas2d.removeEventListener('mousedown', this.handleMouseDown)
     this.canvas2d.removeEventListener('mousemove', this.handleMouseMove)
     this.canvas2d.removeEventListener('mouseup', this.handleMouseUp)
   },
+  // Removes event listeners for 2D canvas.
 
+
+
+  // Removes 3D scene components.
   remove3DScene() {
     if (this.renderer) {
       cancelAnimationFrame(this.animationId)
       this.renderer.dispose()
       this.renderer = null
-      this.scene.dispose() //here check
+      this.scene.dispose() 
       this.scene = null
       this.camera = null
       this.controls = null
     }
   },
+  // Removes 3D scene components.
 
+
+
+  // Draws the initial 2D scene.
   draw2DScene() {
     this.canvas2d.width = window.innerWidth
     this.canvas2d.height = window.innerHeight
     this.redrawCanvas()
   },
+  // Draws the initial 2D scene.
 
+
+
+
+
+  // Methods for drawing and managing 2D canvas.
   drawTempLine(startX, startY, endX, endY) {
+
+    // Clear the entire canvas to remove any previous drawings
     this.tool.clearRect(0, 0, this.canvas2d.width, this.canvas2d.height)
+
+    // Redraw the canvas with existing permanent lines
     this.redrawCanvas()
+
     this.tool.beginPath()
     this.tool.moveTo(startX, startY)
     this.tool.lineTo(endX, endY)
@@ -216,6 +279,8 @@ const DrawingApp = {
       )
     })
   },
+  // Methods for drawing and managing 2D canvas.
+
 
   undo() {
     if (this.undoStack.length > 0) {
@@ -241,22 +306,38 @@ const DrawingApp = {
     }
   },
 
+
+
+  //3D Rendering Methods:
+
+
+  
   createLineGeometries() {
+
+    //This line calculates the center of the canvas in the 3D scene 
     const canvasCenterX = this.canvas3d.width / 2
     const canvasCenterY = this.canvas3d.height / 2
-    const scaleFactor = 0.02
+    
+    // A higher value will make the lines(wall) appear larger.
+    const scaleFactor = 0.01
+
+
     const lineHeight = 2 // Predefined line height
     const lineWidthFactor = 5 // Factor to multiply with pencil width
     const lineGeometries = []
 
     this.lines.forEach((line) => {
+
+      //Calculates the coordinates of the line, adjusting for the canvas center and scaling factor.
       const startX = (line.startX - canvasCenterX) * scaleFactor
       const startY = (canvasCenterY - line.startY) * scaleFactor
       const endX = (line.endX - canvasCenterX) * scaleFactor
       const endY = (canvasCenterY - line.endY) * scaleFactor
 
       const lineShape = new THREE.Shape()
+
       const lineWidth = line.width * scaleFactor * lineWidthFactor
+
       const halfLineWidth = lineWidth / 2
 
       // Calculate the normal vector to the line
@@ -296,6 +377,9 @@ const DrawingApp = {
       })
       const lineMesh = new THREE.Mesh(extrudeGeometry, lineMaterial)
 
+        // Rotate the line mesh to make it face upwards
+        lineMesh.rotateX(Math.PI / 2);
+
       lineGeometries.push(lineMesh)
     })
 
@@ -304,10 +388,10 @@ const DrawingApp = {
   draw3DScene() {
     this.scene = new THREE.Scene()
     this.camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
+      75, //FOV
+      window.innerWidth / window.innerHeight, //Aspect Ratio
+      0.1, //Near : Objects closer than 0.1 units will not be rendered.
+      1000 //Far : Objects farther than 1000 units will not be rendered.
     )
     this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas3d })
     this.renderer.setSize(window.innerWidth, window.innerHeight)
@@ -318,8 +402,8 @@ const DrawingApp = {
       this.scene.add(lineMesh)
     })
 
-    this.camera.position.y = 3
-    this.camera.position.z = 8
+    this.camera.position.y = 2;
+    this.camera.position.z = 8;
 
     this.controls = new OrbitControls(this.camera, this.canvas3d)
     this.controls.enableDamping = true
@@ -341,12 +425,13 @@ const DrawingApp = {
         this.scene.remove(child)
       })
 
-      const lineGeometries = this.createLineGeometries()
+      const lineGeometries = this.createLineGeometries() //Calls the createLineGeometries method to generate new 3D line geometries based on the updated data.
       lineGeometries.forEach((lineMesh) => {
         this.scene.add(lineMesh)
       })
     }
   },
+  //3D Rendering Methods:
 }
 
 DrawingApp.init()
